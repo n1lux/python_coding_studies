ae2fb1c3e1970c3f196eb138c359e9d905908a10
n1lux <n1lux.comp@gmail.com>
2015-05-27 22:00:12 -0300

    First Commit

---
+++ mongoengine/mongo.py
@@ -0,0 +1,30 @@
+from mongoengine import *
+
+connect("tumbleblog")
+
+class User(Document):
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    author = ReferenceField(User)  # These are similar to foreign key fields in traditional ORMs
+    tags = ListField(StringField(max_length=30))
+    comments = ListField(EmbeddedDocument(Comment))
+
+    meta = {'allow_inheritance': True}
+
+class TextPost(Post):
+    content = StringField()
+
+class ImagePost(Post):
+    image_path = StringField()
+
+class LinkPost(Post):
+    link_url = StringField()
+
+
+class Comment(EmbeddedDocument):
+    content = StringField()
+    name = StringField(max_length=120)
